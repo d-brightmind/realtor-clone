e0655b459be63e354aa0d082ab5842ae14d48edb
d-brightmind <robiuridwanullah123@gmail.com>
2024-01-04 19:02:31 +0100

    create private route for protecting profile page and logout functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
     <>
@@ -16,7 +17,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Routes>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { getAuth } from 'firebase/auth';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router';
 
 export default function Profile() {
+  const auth = getAuth();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const {name, email} = formData;
+  const navigate = useNavigate();
+  function onLogOut(){
+    auth.signOut();
+    navigate('/');
+  }
   return (
-    <div>Profile</div>
+    <>
+      <section className='flex justify-center items-center mx-auto max-w-6xl flex-col'>
+        <h1 className='text-3xl text-center font-bold mt-6'>My Profile</h1>
+        <div className='w-full md:w-[50%] mt-6 px-3'>
+          <form>
+            <input type="text" id='name' value={name} className='w-full px-4 py-2 text-xl bg-white text-gray-700 border border-gray-300 transition ease-in-out duration-200 rounded mb-6' disabled/>
+            <input type="email" id='email' value={email} className='w-full px-4 py-2 text-xl bg-white text-gray-700 border border-gray-300 transition ease-in-out duration-200 rounded mb-6' disabled/>
+
+            <div className='flex justify-between sm:text-lg text-sm mb-6 whitespace-nowrap'>
+              <p className='flex items-center'>Do you want to change your name?
+                <span className='text-red-600 hover:text-red-700 ml-1 transition ease-in-out duration-200 cursor-pointer'>Edit</span>
+              </p>
+              <p onClick={onLogOut} className='text-blue-600 hover:text-blue-700  transition ease-in-out duration-200 cursor-pointer'>Sign out</p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   )
 }
